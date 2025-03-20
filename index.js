1c204ecd6e4d9a7ad7238273d13ad6a24e0b7653
Ankit <tyagiiankit@gmail.com>
2025-03-20 21:38:28 +0530

    first-sssssss

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express=require("express");
 const app=express();
 
-const userModel =require("./models/user.js")
+const userModel =require("./model/user.js")
 require("dotenv").config();
 
+app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("hello tajpur");
 
 });
 
+app.get("/create",async(req,res)=>{
+    const user= await userModel.create(req.body);
+    res.send(user);
 
+})
 
 app.listen(3000,()=>{
     console.log("ankit server");
