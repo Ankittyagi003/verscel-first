42816a7d948f2223782c20d1c240a9120cdd0cd4
Ankit <tyagiiankit@gmail.com>
2025-03-21 11:44:00 +0530

    first-sssseeee

--- model/user.js
+++ model/user.js
@@ -1,7 +1,12 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.DB);
+mongoose.connect(process.env.DB).then(()=>{
+    console.log("mongodb conneted");
+
+}).catch((err)=>{
+    console.log("error while miongoose ankit");
+})
 
 const userSchema=mongoose.Schema({
 name:String,
